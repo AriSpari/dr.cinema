9012204a86d044bb3145d2f903bc957810dd8f17
Ari Þórðarson <arith24@ru.is>
2025-12-08 17:53:17 +0000

    api-integration started, added cinemas view(empy)

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,14 +1,39 @@
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList} from "react-native";
 import { useRouter } from "expo-router";
 import styles from "./styles";
+import { fetchMovies } from "@/src/services/get_movies";
+import { useEffect, useState } from "react";
+
 
 export function Main() {
   const router = useRouter();
+
+  const [movies, setMovies] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetchMovies()
+      .then(data => {
+        setMovies(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <View
       style={styles.container}>
         <Text>Main Screen</Text>
-      </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push("/cinemas")}>
+          <Text style={styles.buttonText}>Go to Details</Text>
+        </TouchableOpacity>
+    </View>
   )
 
 }
