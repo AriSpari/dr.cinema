d1af4f21e9c0ceda3790718963c2711d8b73ed33
Ari Þórðarson <arith24@ru.is>
2025-12-08 12:09:04 +0000

    copied main screen from contactor

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,1 +1,19 @@
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import { useRouter } from "expo-router";
 
+export function Main() {
+  const router = useRouter();
+  return (   
+     <View style={styles.container}>
+        {/* <Image style={styles.image} source={require("../../../assets/images/contactor_logo.png")} /> */}
+        <Text>The best app around.</Text>
+        <TouchableOpacity
+          
+          style = {styles.button}
+        >
+          <Text>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    );
+}
